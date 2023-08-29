499f5c4c0a19548582264032fe6a956f489cb9ff
jpcchaves <jpcchaves@outlook.com>
2023-08-29 13:47:16 -0300

    feat: add card to render shopping carts

--- src/modules/shoppingCart/pages/shoppingCartList/index.tsx
+++ src/modules/shoppingCart/pages/shoppingCartList/index.tsx
@@ -1,11 +1,12 @@
+import { Button, Card, CardBody, CardFooter, CardHeader, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { http } from "../../../../http/http";
-import { Box, Heading } from "@chakra-ui/react";
 
 interface IShoppingCart {
   id?: string;
   uuid?: string;
   name: string;
+  productsAmount: string;
   createdAt: string;
 }
 
@@ -27,16 +28,40 @@ const ShoppingCartList = () => {
       .catch((err) => console.log(err));
   };
 
+  const formalizeDate = (rawDate: string) => {
+    return new Date(rawDate);
+  }
+
+  const formatDate = (date: Date) => {
+    const options = { timeZone: 'America/Sao_Paulo' };
+    return date.toLocaleDateString('pt-BR', options);
+  }
+
   return (
-    <>
-      {(shoppingCarts || []).map(({ id, uuid, createdAt, name }) => (
-        <Box>
-          <Heading>
-            {name}, {id}, {uuid}, {createdAt}
-          </Heading>
-        </Box>
+    <SimpleGrid columns={4} gap={4} p={12}>
+      {(shoppingCarts || []).map(({ id, name, productsAmount, createdAt }) => (
+        <>
+          <Card key={id} size={'md'}>
+            <CardHeader>
+              <Heading size='md'>{name}</Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>{name}</Text>
+              <Text>Produtos: {productsAmount}</Text>
+            </CardBody>
+            <CardFooter>
+              <Button w={'full'} colorScheme="blue">
+                Ver mais
+              </Button>
+              <span>
+
+                <small>Criado em {(formatDate(formalizeDate(createdAt)))}</small>
+              </span>
+            </CardFooter>
+          </Card>
+        </>
       ))}
-    </>
+    </SimpleGrid>
   );
 };
 
