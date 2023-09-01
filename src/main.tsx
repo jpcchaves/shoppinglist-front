08d715eb50741762304be7ec10e929a175deb662
João Paulo <jpcchaves@outlook.com>
2023-08-31 22:18:52 -0300

    feat: define redux provider default store

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import {BrowserRouter} from "react-router-dom";
+import {ChakraProvider} from "@chakra-ui/react";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <App />
+        <Provider store={store}>
+          <App/>
+        </Provider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
