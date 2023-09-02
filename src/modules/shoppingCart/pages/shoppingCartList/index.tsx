b3b87dc5e03f4e59573e4e099f83f2c314f8ea00
João Paulo <jpcchaves@outlook.com>
2023-09-02 18:42:51 -0300

    feat: used modal hook and passed as props

--- src/modules/shoppingCart/pages/shoppingCartList/index.tsx
+++ src/modules/shoppingCart/pages/shoppingCartList/index.tsx
@@ -2,17 +2,20 @@ import {useEffect} from "react";
 import useShoppingCart from "../../hook/useShoppingCart";
 import {useAppSelector} from "../../../../hooks/useRedux";
 import ShoppingCartListView from "./view";
+import useShoppingCartModal from "../../hook/useShoppingCartModal";
 
 const ShoppingCartList = () => {
   const {getShoppingCarts} = useShoppingCart();
+  const {isModalOpen, toggleModal} = useShoppingCartModal();
   const {shoppingCarts} = useAppSelector(state => state.shoppingCart);
 
   useEffect(() => {
     getShoppingCarts();
-  }, [getShoppingCarts]);
+  }, []);
 
 
-  return <ShoppingCartListView shoppingCarts={shoppingCarts}/>
+  return <ShoppingCartListView isModalOpen={isModalOpen}
+                               toggleModal={toggleModal} shoppingCarts={shoppingCarts}/>
 };
 
 export default ShoppingCartList;
