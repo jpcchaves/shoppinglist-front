d85f8667be2c30fb97bf90d37d1da78a3cea4c85
João Paulo <jpcchaves@outlook.com>
2023-09-02 18:43:32 -0300

    feat: add new attributes in IProps interface and passed them to the modal

--- src/modules/shoppingCart/pages/shoppingCartList/view.tsx
+++ src/modules/shoppingCart/pages/shoppingCartList/view.tsx
@@ -1,32 +1,60 @@
-import {Button, Card, CardBody, CardFooter, CardHeader, Heading, SimpleGrid, Text, VStack} from "@chakra-ui/react";
-import {formalizeDate, formatDate} from "../../utils/dateUtils";
-import {ShoppingCartModel} from "../../models/ShoppingCartModel";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { formalizeDate, formatDate } from "../../utils/dateUtils";
+import { ShoppingCartModel } from "../../models/ShoppingCartModel";
 import FloatButton from "../../components/floatButton";
+import ShoppingCartFormModal from "../../components/shoppingCartFormModal";
 
 interface IProps {
-  shoppingCarts: ShoppingCartModel[]
+  shoppingCarts: ShoppingCartModel[];
+  isModalOpen: boolean;
+  toggleModal: () => void;
 }
 
-const ShoppingCartListView = ({shoppingCarts}: IProps) => {
+const ShoppingCartListView = ({
+  shoppingCarts,
+  toggleModal,
+  isModalOpen,
+}: IProps) => {
   return (
-    <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} gap={4} p={12}>
-      {(shoppingCarts || []).map(({id, name, productsAmount, createdAt}) => (
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} gap={4} p={12}>
+      <ShoppingCartFormModal
+        isModalOpen={isModalOpen}
+        toggleModal={toggleModal}
+      />
+
+      {(shoppingCarts || []).map(({ id, name, productsAmount, createdAt }) => (
         <>
-          <Card key={id} size={'md'}>
+          <Card key={id} size={"md"}>
             <CardHeader>
-              <Heading size='md'>{name}</Heading>
+              <Heading size="md">{name}</Heading>
             </CardHeader>
             <CardBody>
               <Text>{name}</Text>
               <Text>Produtos: {productsAmount}</Text>
             </CardBody>
             <CardFooter>
-              <VStack w={'full'}>
-                <Button w={'full'} colorScheme="blue">
+              <VStack w={"full"}>
+                <Button w={"full"} colorScheme="blue">
                   Ver mais
                 </Button>
-                <Text w={'full'} textAlign={'end'} fontSize='x-small' fontStyle={'italic'} color={'whiteAlpha.500'}>
-                  Criado em {(formatDate(formalizeDate(createdAt)))}
+                <Text
+                  w={"full"}
+                  textAlign={"end"}
+                  fontSize="x-small"
+                  fontStyle={"italic"}
+                  color={"whiteAlpha.500"}
+                >
+                  Criado em {formatDate(formalizeDate(createdAt))}
                 </Text>
               </VStack>
             </CardFooter>
@@ -34,7 +62,7 @@ const ShoppingCartListView = ({shoppingCarts}: IProps) => {
         </>
       ))}
 
-      <FloatButton aria-label={'float button'}/>
+      <FloatButton handleClick={toggleModal} aria-label={"float button"} />
     </SimpleGrid>
   );
 };
