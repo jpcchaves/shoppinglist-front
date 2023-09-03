8382e78e58c63bcbf0a3965f65a40bf0461a1219
João Paulo <jpcchaves@outlook.com>
2023-09-03 10:37:44 -0300

    feat: add create method

--- src/modules/shoppingCart/hook/useShoppingCart.tsx
+++ src/modules/shoppingCart/hook/useShoppingCart.tsx
@@ -1,22 +1,48 @@
-import {http} from "../../../http/http";
-import {ShoppingCartModel} from "../models/ShoppingCartModel";
-import {useAppDispatch} from "../../../hooks/useRedux";
-import {loadShoppingCarts} from "../../../store/shoppingCart/shoppingCartSlice";
+import { http } from "../../../http/http";
+import { ShoppingCartModel } from "../models/ShoppingCartModel";
+import { useAppDispatch } from "../../../hooks/useRedux";
+import { loadShoppingCarts } from "../../../store/shoppingCart/shoppingCartSlice";
+import useToast from "../../../hooks/useToast";
+import { FormikValues } from "formik";
 
-const UseShoppingCart = () => {
+interface IProps {
+  validation: FormikValues;
+  toggleModal: () => void;
+}
 
+const UseShoppingCart = ({ validation, toggleModal }: IProps) => {
   const dispatch = useAppDispatch();
+  const { notifySuccess, notifyError } = useToast();
+
+  const resetForm = (): void => {
+    validation.resetForm();
+  };
 
   const getShoppingCarts = () => {
     http
       .get("/api/v1/shopping-carts")
-      .then(({data}: { data: ShoppingCartModel[] }) => {
+      .then(({ data }: { data: ShoppingCartModel[] }) => {
         dispatch(loadShoppingCarts(data));
       })
       .catch((err) => console.log(err));
   };
 
-  return {getShoppingCarts}
+  const createShoppingCart = (data: ShoppingCartModel) => {
+    http
+      .post("/api/v1/shopping-carts", data)
+      .then(() => {
+        getShoppingCarts();
+        notifySuccess("Lista de compras criada com sucesso!");
+        toggleModal();
+        resetForm();
+      })
+      .catch((err) => {
+        console.log(err);
+        notifyError("Ocorreu um erro ao criar a lista de compras");
+      });
+  };
+
+  return { getShoppingCarts, createShoppingCart };
 };
 
 export default UseShoppingCart;
