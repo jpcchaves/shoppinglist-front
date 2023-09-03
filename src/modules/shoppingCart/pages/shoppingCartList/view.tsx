cc6386bd59621578af25ffbb558bcd3663b03bf0
João Paulo <jpcchaves@outlook.com>
2023-09-03 11:33:09 -0300

    feat: add popover with actions

--- src/modules/shoppingCart/pages/shoppingCartList/view.tsx
+++ src/modules/shoppingCart/pages/shoppingCartList/view.tsx
@@ -1,10 +1,18 @@
 import {
+  Box,
   Button,
   Card,
   CardBody,
   CardFooter,
   CardHeader,
+  Flex,
   Heading,
+  IconButton,
+  Popover,
+  PopoverArrow,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
   SimpleGrid,
   Text,
   VStack,
@@ -14,6 +22,9 @@ import { ShoppingCartModel } from "../../models/ShoppingCartModel";
 import FloatButton from "../../components/floatButton";
 import ShoppingCartFormModal from "../../components/shoppingCartFormModal";
 import { FormikValues } from "formik";
+
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 
 interface IProps {
   shoppingCarts: ShoppingCartModel[];
@@ -37,33 +48,69 @@ const ShoppingCartListView = ({
       />
 
       {(shoppingCarts || []).map(({ id, name, productsAmount, createdAt }) => (
-        <>
-          <Card key={id} size={"md"}>
-            <CardHeader>
-              <Heading size="md">{name}</Heading>
-            </CardHeader>
-            <CardBody>
-              <Text>{name}</Text>
-              <Text>Produtos: {productsAmount}</Text>
-            </CardBody>
-            <CardFooter>
-              <VStack w={"full"}>
-                <Button w={"full"} colorScheme="blue">
-                  Ver mais
-                </Button>
-                <Text
-                  w={"full"}
-                  textAlign={"end"}
-                  fontSize="x-small"
-                  fontStyle={"italic"}
-                  color={"whiteAlpha.500"}
-                >
-                  Criado em {formatDate(createdAt!)}
-                </Text>
-              </VStack>
-            </CardFooter>
-          </Card>
-        </>
+        <Card key={id} size={"md"} variant={"filled"}>
+          <CardHeader>
+            <Flex gap={"4"}>
+              <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+                <Box>
+                  <Heading size="sm">{name}</Heading>
+                </Box>
+              </Flex>
+
+              <Popover placement={"left-end"}>
+                <PopoverTrigger>
+                  <IconButton
+                    variant="link"
+                    colorScheme="gray"
+                    aria-label="menu options"
+                    icon={<BsThreeDotsVertical />}
+                  />
+                </PopoverTrigger>
+                <PopoverContent zIndex={4} w={"fit-content"} textAlign={"left"}>
+                  <PopoverHeader>
+                    <Button
+                      aria-label={"edit button"}
+                      variant={"link"}
+                      leftIcon={<EditIcon color={"green.300"} />}
+                    >
+                      Editar
+                    </Button>
+                  </PopoverHeader>
+                  <PopoverHeader>
+                    <Button
+                      aria-label={"edit button"}
+                      variant={"link"}
+                      leftIcon={<DeleteIcon color={"red.300"} />}
+                    >
+                      Apagar
+                    </Button>
+                  </PopoverHeader>
+                  <PopoverArrow />
+                </PopoverContent>
+              </Popover>
+            </Flex>
+          </CardHeader>
+          <CardBody>
+            <Text>{name}</Text>
+            <Text>Produtos: {productsAmount}</Text>
+          </CardBody>
+          <CardFooter>
+            <VStack w={"full"}>
+              <Button w={"full"} colorScheme="blue">
+                Ver mais
+              </Button>
+              <Text
+                w={"full"}
+                textAlign={"end"}
+                fontSize="x-small"
+                fontStyle={"italic"}
+                color={"whiteAlpha.500"}
+              >
+                Criado em {formatDate(createdAt!)}
+              </Text>
+            </VStack>
+          </CardFooter>
+        </Card>
       ))}
 
       <FloatButton handleClick={toggleModal} aria-label={"float button"} />
