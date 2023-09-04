338df90cfe44817617f38b34905add0c4c3ab16b
João Paulo <jpcchaves@outlook.com>
2023-09-04 14:09:33 -0300

    feat: add reducer methods to hold shopping cart by id and clear it too

--- src/store/shoppingCart/shoppingCartSlice.ts
+++ src/store/shoppingCart/shoppingCartSlice.ts
@@ -1,27 +1,45 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {ShoppingCartModel} from "../../modules/shoppingCart/models/ShoppingCartModel";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ShoppingCartModel } from "../../modules/shoppingCart/models/ShoppingCartModel";
 
 interface ShoppingCartState {
   shoppingCarts: ShoppingCartModel[];
+  shoppingCartById: ShoppingCartModel;
 }
 
-interface PayloadLoadShoppingCartModel extends PayloadAction<ShoppingCartModel[]> {
-}
+interface PayloadLoadShoppingCart extends PayloadAction<ShoppingCartModel[]> {}
 
+interface PayloadLoadShoppingCartById
+  extends PayloadAction<ShoppingCartModel> {}
 
 const initialState: ShoppingCartState = {
   shoppingCarts: [],
+  shoppingCartById: null!,
 };
 
 export const shoppingCartSlice = createSlice({
   name: "shoppingCart",
   initialState,
   reducers: {
-    loadShoppingCarts: (state: ShoppingCartState, action: PayloadLoadShoppingCartModel) => {
+    loadShoppingCarts: (
+      state: ShoppingCartState,
+      action: PayloadLoadShoppingCart,
+    ) => {
       state.shoppingCarts = action.payload;
+    },
+    loadShoppingCartById: (
+      state: ShoppingCartState,
+      action: PayloadLoadShoppingCartById,
+    ) => {
+      state.shoppingCartById = action.payload;
+    },
+    clearShoppingCartById: (state: ShoppingCartState) => {
+      state.shoppingCartById = null!;
     },
   },
 });
 
-export const {loadShoppingCarts} =
-  shoppingCartSlice.actions;
+export const {
+  loadShoppingCarts,
+  loadShoppingCartById,
+  clearShoppingCartById,
+} = shoppingCartSlice.actions;
