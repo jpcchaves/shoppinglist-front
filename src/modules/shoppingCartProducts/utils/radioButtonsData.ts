4277b22b1489c7d6ff1984372f050376ecfb10d3
João Paulo <jpcchaves@outlook.com>
2023-09-07 15:03:24 -0300

    feat: add radio buttons data

---
+++ src/modules/shoppingCartProducts/utils/radioButtonsData.ts
@@ -0,0 +1,17 @@
+export const radioButtons = [
+  {
+    id: Math.random(),
+    color: "facebook",
+    value: "LOWEST",
+    label: "Talvez Comprar",
+  },
+  { id: Math.random(), color: "blue", value: "LOW", label: "Pode Esperar" },
+  { id: Math.random(), color: "green", value: "MEDIUM", label: "Normal" },
+  { id: Math.random(), color: "yellow", value: "HIGH", label: "Urgente" },
+  {
+    id: Math.random(),
+    color: "red",
+    value: "CRITICAL",
+    label: "Muito Urgente",
+  },
+];
