4bd1a1d7bb34b8efb20f1e28b272a0fb83d1e2de
João Paulo <jpcchaves@outlook.com>
2023-09-07 13:18:48 -0300

    feat: add products table layout

--- src/modules/shoppingCartProducts/pages/productsList/view.tsx
+++ src/modules/shoppingCartProducts/pages/productsList/view.tsx
@@ -1,16 +1,64 @@
 import { useAppSelector } from "../../../../hooks/useRedux";
+import {
+  Heading,
+  IconButton,
+  SimpleGrid,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import FloatButton from "../../../shoppingCart/components/floatButton";
 
 const ProductsListView = () => {
   const { productList } = useAppSelector((state) => state.product);
 
   return (
-    <div>
-      {(productList || []).map(({ id, name, urgencyLevel }) => (
-        <p key={id}>
-          {name} {urgencyLevel}
-        </p>
-      ))}
-    </div>
+    <SimpleGrid pt={16} px={{ base: "8", md: "16", lg: "32", xl: "48" }}>
+      <Heading textAlign={"center"} pb={8}>
+        Lista de Compras
+      </Heading>
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th textAlign={"center"}>Produtos</Th>
+              <Th textAlign={"center"}>Nível de Urgência</Th>
+              <Th></Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {(productList || []).map(({ id, name, urgencyLevel }) => (
+              <Tr key={id}>
+                <Td textAlign={"center"}>{name}</Td>
+                <Td textAlign={"center"}>{urgencyLevel}</Td>
+                <Td textAlign={"end"}>
+                  <IconButton
+                    variant="link"
+                    aria-label={"action button"}
+                    icon={<EditIcon />}
+                  />
+                  <IconButton
+                    variant="link"
+                    aria-label={"action button"}
+                    icon={<DeleteIcon />}
+                  />
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+
+      <FloatButton
+        handleClick={() => console.log("")}
+        aria-label={"float button"}
+      />
+    </SimpleGrid>
   );
 };
 
