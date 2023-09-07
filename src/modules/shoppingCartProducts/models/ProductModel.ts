9ee68ba9be086680067d368d8d4a956d374fbd04
João Paulo <jpcchaves@outlook.com>
2023-09-07 15:02:14 -0300

    feat: adjusted product model and create urgency level enum

--- src/modules/shoppingCartProducts/models/ProductModel.ts
+++ src/modules/shoppingCartProducts/models/ProductModel.ts
@@ -1,10 +1,8 @@
+import { UrgencyLevel } from "./urgencyLevel";
+
 export type ProductModel = {
   id?: string;
   name: string;
-  urgencyLevel:
-    | "Muito Urgente"
-    | "Urgente"
-    | "Normal"
-    | "Pode Esperar"
-    | "Talvez Comprar";
+  urgencyLevel: UrgencyLevel;
+  shoppingCartId?: string;
 };
---
+++ src/modules/shoppingCartProducts/models/urgencyLevel.ts
@@ -0,0 +1,7 @@
+export enum UrgencyLevel {
+  CRITICAL = "CRITICAL",
+  HIGH = "HIGH",
+  NORMAL = "MEDIUM",
+  LOW = "LOW",
+  LOWEST = "LOWEST",
+}
