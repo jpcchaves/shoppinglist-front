697ee9babec61b7c48b3ee7270bbf4cbb441ec3a
João Paulo <jpcchaves@outlook.com>
2023-09-23 23:16:42 -0300

    refactor: adjusted format currency to always show 2 decimals

--- src/modules/shoppingCartProducts/utils/formatCurrency.ts
+++ src/modules/shoppingCartProducts/utils/formatCurrency.ts
@@ -2,8 +2,19 @@ export const replaceDot = (valueWithDot: string): string => {
   return valueWithDot.replace(".", ",");
 }
 
+export const replaceSemiColon = (valueWithSemiColon: string): string => {
+  return valueWithSemiColon.replace(",", ".");
+}
+
 export const formatCurrency = (rawValue: string | undefined): string => {
-    return `R$ ${rawValue}`;
+    const rawValueNumber =  Number(rawValue);
+
+    const formattedValue = rawValueNumber.toFixed(2);
+    const [wholePart, decimalPart] = formattedValue.split('.');
+    const formattedWholePart = wholePart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    return `R$ ${formattedWholePart},${decimalPart}`;
+
 }
 
 
